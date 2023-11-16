3f0675093e8a4c66cda8780dd6b32b6af45e5706
KarenPineda5 <150054690+KarenPineda5@users.noreply.github.com>
2023-11-15 23:57:17 -0600

    Update and rename Personal.java to Ganancias.java

--- proyectoEmpresas/src/proyectoempresas/src/main/java/empresas/example/Ganancias.java
+++ proyectoEmpresas/src/proyectoempresas/src/main/java/empresas/example/Ganancias.java
@@ -8,20 +8,39 @@ import javax.swing.JLabel;
 /**
  * Representa el personal de la empresa.
  */
-public class Personal {
+public class Ganancias {
 
     private JFrame frame;
         
 
-    public Personal() {
-        frame = new JFrame("Presupuesto");
+    public Ganancias() {
+        frame = new JFrame("Ganancias");
         frame.setSize(700, 500);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         // Título
-        JLabel titleLabel = new JLabel("Personal");
+        JLabel titleLabel = new JLabel("Ganancias");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        // Crear un arreglo bidimensional
+            int[][] tabla = new int[5][5];
+            
+    
+            // Llenar la tabla
+            for (int i = 0; i < tabla.length; i++) {
+                for (int j = 0; j < tabla[i].length; j++) {
+                    tabla[i][j] = i * j;
+                }
+            }
+    
+            // Imprimir la tabla
+            for (int i = 0; i < tabla.length; i++) {
+                for (int j = 0; j < tabla[i].length; j++) {
+                    System.out.print(tabla[i][j] + " ");
+                }
+                System.out.println();
+            }
 
         // represar a página principal
         JButton backButton = new JButton("Regresar");
