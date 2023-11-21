685754c06d6c8ba69c65f5f57af80647134eb430
Paul-1511 <139778187+Paul-1511@users.noreply.github.com>
2023-11-21 17:19:19 -0600

    Update Equilibrio.java

--- proyectoEmpresas/src/proyectoempresas/src/main/java/empresas/example/Equilibrio.java
+++ proyectoEmpresas/src/proyectoempresas/src/main/java/empresas/example/Equilibrio.java
@@ -1,37 +1,81 @@
 package empresas.example;
 
 import java.awt.BorderLayout;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.apache.commons.collections4.Get;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
- * Representa el punto de equilibrio de la empresa.
+ * Representa al inventario de la empresa.
  */
 public class Equilibrio {
-    private JFrame frame;
-    private String username;
-    private JTable table;
-    private DefaultTableModel model;
+        private JFrame frame;
+        private String companyName;
+        private String username;
+        private JTextField nameEmpresa;
+        private GetInfo getInfoPanel;
+        private JTextArea result_area;
 
-    public Equilibrio(String username) {
-        this.username = username;
-
-        frame = new JFrame("Punto de Equilibrio");
+    public Equilibrio() {
+        frame = new JFrame("Perfil");
         frame.setSize(700, 500);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        // Título
-        JLabel titleLabel = new JLabel("Punto de Equilibrio");
+        // título
+        JLabel titleLabel = new JLabel("Perfil");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        
+        Font questionFont = new Font( "Trebuchet MS", Font.BOLD, 16 );  // estilo preguntas
+        Font textFieldFont = new Font( "Trebuchet MS", Font.PLAIN , 18);
+
+        JPanel inputPanel = new JPanel(new GridBagLayout()); // grid para desplegar la info
+        GridBagConstraints griid = new GridBagConstraints();
+        griid.insets = new Insets(5, 5, 5 , 5 ); 
+        
+        getInfoPanel = new GetInfo(companyName) ;
+        getInfoPanel.setVisible(false );
+
+        griid.gridx = 0;
+        griid.gridy = 9 ; // se coloca abajo
+        griid.anchor = GridBagConstraints.EAST;
+        JLabel companyLabel =  new JLabel ( "Nombre de la empresa:" );
+        companyLabel.setFont(questionFont);
+        inputPanel.setBackground(Color.BLACK);
+        companyLabel.setForeground(Color.WHITE);
+        inputPanel.add(companyLabel , griid );
+
+        griid.gridx = 1;
+        griid.anchor = GridBagConstraints.WEST;
+        nameEmpresa =  new JTextField();
+        nameEmpresa.setBackground( Color.BLACK);
+        nameEmpresa.setForeground( Color.WHITE);
+        nameEmpresa.setFont(textFieldFont);
+        nameEmpresa.setPreferredSize(new Dimension(200, 35)); // tamaño del campo de nombre
+        inputPanel.add(nameEmpresa, griid);
+
+        GetInfo getInfo = new GetInfo(companyName);
+
+        result_area.setText("El punto de equilibrio es: "  + GetInfo.breakEvenPointUnits +  " unidades.\n");
+            result_area.append("Total de costos fijos: " + GetInfo.totalFixedCosts  + "\n");
+            result_area.append("Costo variable por unidad: "   + GetInfo.variableCostsPerUnit + "\n");
+            result_area.append("Precio por unidad: " + GetInfo.pricePerUnit +    "\n" );
 
         // para regresar a la página principal
         JButton backButton = new JButton("Regresar");
@@ -42,22 +86,11 @@ public class Equilibrio {
             controlador.MainPage(); 
         });
 
-        // estilo
+        // estilo y centrar
         frame.setLayout(new BorderLayout());
         frame.add(titleLabel, BorderLayout.CENTER);
         frame.add(backButton, BorderLayout.SOUTH);
 
-
-        //datos
-        cargarDatos();
-
         frame.setVisible(true);
-    }    
-
-    private void cargarDatos() {
-        String path = "empresa_info.csv"; 
-        String line = "";
     }
-    
-    
 }
