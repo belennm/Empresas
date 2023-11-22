1b052931dc485452f9c560e749e60150edb0a2a0
belennm <132374004+belennm@users.noreply.github.com>
2023-11-21 18:53:31 -0600

    Delete BreakEvenPoint directory

--- BreakEvenPoint/App.java
+++
@@ -1,8 +0,0 @@
-package BreakEvenPoint;
-
-public class App {
-    public static void main(String[] agrs){
-        Controlador controlador = new Controlador();
-        controlador.iniciar();
-    }
-}
